b7e118bf21e5b0d974bd2d326e9eaef50bb33c08
DamanbirSingh199 <damanbirsingh199@gmail.com>
2018-11-16 22:02:36 +0530

    Start

---
+++ datastructures.R
@@ -0,0 +1,25 @@
+#equals to
+x1= 3
+x2<-3
+
+x1
+x2
+x1+x2
+men
+?women
+data()
+trees
+?AirPassengers
+library
+?mean
+x= c(0:10, 50)
+xm= mean(x)
+c(xm, mean(x, trim = 0.10))
+??trim
+??mean
+
+x= c(0:1,3)
+x
+getwd()
+plot(1:50)
+plot(x)
--- file1.R
+++ file1.R
@@ -1,1 +1,3 @@
 women
+git config --global user.email "damanbirsingh199@gmail.com"
+git config --global user.name "DamanbirSingh199"
